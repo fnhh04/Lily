ee3879151e4a85b9d9ad1add7e6bcef1ff0f8edd
fnhh04 <84245190+fnhh04@users.noreply.github.com>
2021-07-10 05:27:16 +0100

    Add files via upload

--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,12 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 // const infuraKey = "fj4jll3k.....";
 //
 // const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+require('dotenv').config();
+const privateKeys= process.env.PRIVATE_KEY;
 
 module.exports = {
   /**
@@ -48,31 +49,33 @@ module.exports = {
     //  network_id: "*",       // Any network (default: none)
     // },
     // Another network with more advanced options...
-    // advanced: {
-    // port: 8777,             // Custom port
-    // network_id: 1342,       // Custom network
-    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-    // from: <address>,        // Account to send txs from (default: accounts[0])
-    // websocket: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    bsc: {
+      provider: () => new HDWalletProvider(
+        privateKeys, 
+        'https://bsc-dataseed.binance.org/'
+      ),
+      gasPrice: 5 * 10 ** 9,
+      gas: 8164631,
+      network_id: 56,
+      skipDryRun: true
+    },
+    bscTestnet: {
+      provider: () => new HDWalletProvider(
+        privateKeys, 
+        'https://data-seed-prebsc-1-s1.binance.org:8545'
+      ),
+      gas: 6897127,
+      network_id: 97,
+      skipDryRun: true
+    }
+  },
     // Useful for private networks
     // private: {
     // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
     // network_id: 2111,   // This network is yours, in the cloud.
     // production: true    // Treats this network as if it was a public net. (default: false)
     // }
-  },
+  
 
   // Set default mocha options here, use special reporters etc.
   mocha: {
@@ -93,7 +96,8 @@ module.exports = {
       // }
     }
   },
-
+  
+  
   // Truffle DB is currently disabled by default; to enable it, change enabled: false to enabled: true
   //
   // Note: if you migrated your contracts prior to enabling this field in your Truffle project and want
@@ -102,5 +106,12 @@ module.exports = {
 
   db: {
     enabled: false
-  }
+  },
+
+  plugins: [
+    'truffle-plugin-verify'
+  ],
+ api_keys: {
+   bscscan: process.env.API_KEY
+ }
 };
