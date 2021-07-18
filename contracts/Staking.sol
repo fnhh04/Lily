e084507df3d8df924aeed1526882dcc50b3f15a4
fnhh04 <84245190+fnhh04@users.noreply.github.com>
2021-07-18 05:50:46 +0100

    Add files via upload

---
+++ contracts/Staking.sol
@@ -0,0 +1,76 @@
+pragma solidity ^0.6.12;
+//SPDX-License-Identifier: <SPDX-License>
+/* This contract creates a staking mechanism for the token. 
+To create a token staking mechanism, we need
+1. A staking token
+2. Data structures to keep track of stakes, stakeholders and rewards/penalties (for withdrawal)
+3. Methods to create and remove stakes
+4. A rewards system
+credit: https://hackernoon.com/implementing-staking-in-solidity-1687302a82cf
+*/
+import "./IERC20.sol";
+import './Ownable.sol';
+import './SafeMath.sol';
+import './Fnhh.sol';
+import './sToken.sol';
+
+abstract contract StakingToken is IERC20, Ownable, sToken {
+    using SafeMath for uint256;
+
+	Fnhh public fnhh; // smart contract type
+	sToken public stoken; 
+	
+
+	//array to keep the address of all the stakers --> keep track for transaction 
+	address[] public stakers;
+	mapping(address => uint) public stakingBalance;
+	mapping(address=> bool) public hasStaked;
+
+	event Staking(address indexed owner, uint value, bool hasStaked);
+	event Unstaking(address indexed owner, uint value, bool hasStaked);
+
+	function staking(uint256 _amount) public {
+		fnhh.transferFrom(msg.sender, address(this), _amount);
+		// add to the staking balance (sToken)
+		stakingBalance[msg.sender] = stakingBalance[msg.sender].add(_amount);
+
+		// mint sToken and send to msg.sender
+		_mint(msg.sender, _amount);
+		
+		//add user to stakers array "only if" they haven't staked already
+		if(!hasStaked[msg.sender]) {
+			stakers.push(msg.sender);
+		}
+
+		//update staking status
+		hasStaked[msg.sender] = true;
+		
+		emit Staking(msg.sender, _amount, hasStaked[msg.sender]);
+
+	}
+
+	//allow withdrawal of tokens
+	//deduct 7% 
+	function unstaking(uint256 _amount, uint256 withdrawalfee) public {
+		//  withdrawal fee
+		require(hasStaked[msg.sender]=true, 'no staked amount');
+		require(stakingBalance[msg.sender]> 0, "staking balance cannot be 0" );
+		// reduce the staking balance (sToken) to lower amount
+		stakingBalance[msg.sender]=stakingBalance[msg.sender].sub(_amount);
+		
+		// transfer fnhh token to the sender
+		fnhh.transfer(msg.sender, (_amount.sub(_amount*withdrawalfee)).div(100));
+				
+		// burn the whole amount of sToken that has been redeemed
+		_burn(address(this), _amount);
+
+		//Update staking status
+		if(stakingBalance[msg.sender] ==0) {
+			hasStaked[msg.sender]=false;
+		} else {
+			hasStaked[msg.sender]=true;
+		}
+
+		emit Unstaking(msg.sender, _amount, hasStaked[msg.sender]);
+	}
+}
---
+++ contracts/sToken.sol
@@ -0,0 +1,55 @@
+pragma solidity ^0.6.12;
+
+import './IERC20.sol';
+import './Ownable.sol';
+import './SafeMath.sol';
+
+abstract contract sToken is IERC20 {
+    using SafeMath for uint;
+
+    string internal constant _name = 'Staked Token';
+    string internal constant _symbol = 'STK';
+    uint8 internal constant _decimals = 9;
+
+    mapping(address => uint) private balanceOf;
+    mapping(address => mapping(address => uint)) private allowance;
+
+    event Approval(address indexed owner, address indexed spender, uint value);
+    event Transfer(address indexed from, address indexed to, uint value);
+
+    // return the balance of reflection amount
+
+    function transfer(address to, uint value) external override returns (bool) {
+        balanceOf[msg.sender] = balanceOf[msg.sender].sub(value);
+        balanceOf[to] = balanceOf[to].add(value);
+        emit Transfer(msg.sender, to, value);
+    }
+
+    function approve(address spender, uint value) external override returns (bool) {
+        allowance[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+    }
+
+
+    function transferFrom(address from, address to, uint value) external override returns (bool) {
+        if (allowance[from][msg.sender] != uint(-1)) {
+            allowance[from][msg.sender] = allowance[from][msg.sender].sub(value);
+        }
+        balanceOf[from] = balanceOf[from].sub(value);
+        balanceOf[to] = balanceOf[to].add(value);
+        emit Transfer(from, to, value);
+        return true;
+    }
+
+    function _mint(address to, uint256 amount) internal {
+        require(amount >0, 'invalid amount');
+        balanceOf[to]=balanceOf[to].add(amount);
+        emit Transfer(address(0), to, amount);
+    } 
+
+    function _burn(address from, uint amount) internal {
+        balanceOf[from] = balanceOf[from].sub(amount);
+        emit Transfer(from, address(0), amount);
+    }
+
+}
